cede75155dfb95cdd780731f0ffd67d1bc5f6c89
Ninjaphil24 <mphilippos@hotmail.com>
2024-11-13 22:45:14 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                "hanken-grotesk": ["Hanken Grotesk", "sans-serif"],
+            },
+            colors: {
+                "black": "#060606",
+            },
+            fontSize: {
+                "2xs": ".625rem",
+                "3xs": ".5rem",
+            }
+        },
+    },
+    plugins: [],
+};
