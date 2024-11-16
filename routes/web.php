53c3ab8ba3ce067ba2dfdabde369c526abc631c9
Ninjaphil24 <mphilippos@hotmail.com>
2024-11-16 22:57:28 +0200

    Basic Form setup

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobsController;
+use App\Http\Controllers\SessionController;
+use App\Http\Controllers\RegisteredUserController;
 
 Route::get('/', [JobsController::class, 'index']);
+
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::delete('/logout', [SessionController::class, 'destroy']);
